768349f95c89e81b0c378fb23af21ea051da0fce
hendersonick <nickster9808@gmail.com>
2023-11-13 11:18:59 -0500

    Task 6 revised

--- ui/src/ducks/inventory/index.js
+++ ui/src/ducks/inventory/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-import { openSuccess } from '/Users/nickhenderson/Interclypse/fullstack-starter/ui/src/ducks/alerts/index.js'
 import { createAction, handleActions } from 'redux-actions'
-
+import { openSuccess } from 'src/ducks/alerts/index.js';
 
 const actions = {
   INVENTORY_GET_ALL: 'inventory/get_all',
@@ -12,31 +11,30 @@ const actions = {
 }
 
 export let defaultState = {
-  all: [],
-  fetched: false,
+  all: []
 }
 
-export const findInventory = createAction(actions.INVENTORY_GET_ALL, () =>
-  (dispatch, getState, config) =>
-    axios.get(`${config.restAPIUrl}/inventory`)
-      .then((suc) => dispatch(refreshInventory(suc.data)))
-)
+export const findInventory = createAction(actions.INVENTORY_GET_ALL, () => {
+  (dispatch, getState, config) => axios
+    .get(`${config.restAPIUrl}/inventory`)
+    .then((suc) => dispatch(refreshInventory(suc.data)))
+})
 
 
 export const saveInventory = createAction(actions.INVENTORY_SAVE, (inventory) =>
-  (dispatch, getState, config) => axios
-    .post(`${config.restAPIUrl}/inventory`, inventory)
-    .then((suc) => {
-      const invs = []
-      getState().inventory.all.forEach(inv => {
-        if (inv.id !== suc.data.id) {
-          invs.push(inv)
-        }
-      })
-      invs.push(suc.data)
-      dispatch(refreshInventory(invs))
-      dispatch(openSuccess('Inventory saved successfully.'))
+(dispatch, getState, config) => axios
+  .post(`${config.restAPIUrl}/inventory`, inventory)
+  .then((suc) => {
+    const invs = []
+    getState().inventory.all.forEach(inv => {
+      if (inv.id !== suc.data.id) {
+        invs.push(inv)
+      }
     })
+    invs.push(suc.data)
+    dispatch(refreshInventory(invs))
+    dispatch(openSuccess('Inventory saved successfully.'))
+  })
 )
 
 export const removeInventory = createAction(actions.INVENTORY_DELETE, (ids) =>
@@ -51,6 +49,7 @@ export const removeInventory = createAction(actions.INVENTORY_DELETE, (ids) =>
       })
       dispatch(refreshInventory(invs))
       dispatch(openSuccess('Inventory removed successfully.'))
+
     })
 )
 
